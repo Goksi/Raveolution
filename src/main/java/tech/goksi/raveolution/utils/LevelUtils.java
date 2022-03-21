91a4524f5a6eaff57a44db339ee9d0d246435c59
Goksi <admin@goksi.co.uk>
2022-03-22 00:38:36 +0100

    finally xp system

--- src/main/java/tech/goksi/raveolution/utils/LevelUtils.java
+++ src/main/java/tech/goksi/raveolution/utils/LevelUtils.java
@@ -3,36 +3,10 @@ package tech.goksi.raveolution.utils;
 import java.util.Random;
 
 public class LevelUtils {
-    public static long randomXP(int min, int max){
-        Random r = new Random();
-        return r.nextInt((max - min) + 1) + min;
+    public static long xpToNextLevel(int level) {
+        return 5 * (((long) Math.pow(level, 2)) + 10L * level + 20);
     }
 
-    public static long remainingXp(long totalXp) {
-        int lvl = xpToLevels(totalXp);
-
-        if (lvl == 0) return totalXp;
-
-        long xp = levelsToXp(lvl);
-
-        return totalXp - xp + xpToNextLevel(lvl);
-    }
-    public static int xpToLevels(long totalXp) { //working !!!
-        boolean rac = true;
-        int level = 0;
-
-        while (rac) {
-            long xp = levelsToXp(level);
-
-            if (totalXp < xp) {
-                rac = false;
-            } else {
-                level++;
-            }
-        }
-
-        return level;
-    }
     public static long levelsToXp(int levels) {
         long xp = 0;
 
@@ -42,8 +16,38 @@ public class LevelUtils {
 
         return xp;
     }
-    public static long xpToNextLevel(int level) {
-        return 5 * (((long) Math.pow(level, 2)) + 10L * level);
+
+    public static int xpToLevels(long totalXp) {
+        boolean calculating = true;
+        int level = 0;
+
+        while (calculating) {
+            long xp = levelsToXp(level);
+
+            if (totalXp < xp) {
+                calculating = false;
+            } else {
+                level++;
+            }
+        }
+
+        return level;
+    }
+
+    public static long remainingXp(long totalXp) {
+        int level = xpToLevels(totalXp); //5
+
+        if (level == 0) return totalXp;
+
+        long xp = levelsToXp(level);
+
+        return xp - totalXp;
+    }
+
+    public static int randomXp(int min, int max) {
+        Random random = new Random();
+
+        return random.nextInt((max - min) + 1) + min;
     }
 
 
