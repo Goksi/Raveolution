be521003957b5feb0829c72d9c5206e4af457d6e
Goksi <admin@goksi.co.uk>
2022-03-24 00:45:08 +0100

    small fixes

--- src/main/java/tech/goksi/raveolution/events/XPHandler.java
+++ src/main/java/tech/goksi/raveolution/events/XPHandler.java
@@ -4,16 +4,18 @@ import net.dv8tion.jda.api.entities.User;
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 import org.jetbrains.annotations.NotNull;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import tech.goksi.raveolution.Bot;
 import tech.goksi.raveolution.exceptions.FailFormatException;
 import tech.goksi.raveolution.utils.ConfigUtils;
 import tech.goksi.raveolution.utils.LevelUtils;
 import tech.goksi.raveolution.utils.MillisConvert;
-
 import java.util.*;
 
 public class XPHandler extends ListenerAdapter {
     private final List<User> onCoolDown = new ArrayList<>();
+    private final Logger logger = LoggerFactory.getLogger(XPHandler.class);
 
     @Override
     public void onMessageReceived(@NotNull MessageReceivedEvent event) {
@@ -25,7 +27,8 @@ public class XPHandler extends ListenerAdapter {
             int xpToLvl = LevelUtils.xpToLevels(Bot.getInstance().getDatabase().getXP(event.getAuthor()));
             if(xpToLvl > Bot.getInstance().getDatabase().getLvl(event.getAuthor())){
                 Bot.getInstance().getDatabase().addLvl(event.getAuthor(), xpToLvl);
-                System.out.println("[Raveolution] User " + event.getAuthor().getAsTag() + " has been levelup to level " + xpToLvl);
+                logger.info("[Raveolution] User " + event.getAuthor().getAsTag() + " has been levelup to level " + xpToLvl);
+
                 if(ConfigUtils.getBoolean("XPSystem.lvlUpMsg")){
                     event.getChannel().sendMessage(ConfigUtils.getString("XPSystem.lvlUpMsgStr", "%user%", event.getAuthor().getAsMention()).
                             replaceAll("%level%", String.valueOf(Bot.getInstance().getDatabase().getLvl(event.getAuthor())))).queue();
